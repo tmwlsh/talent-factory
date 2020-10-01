509b93add23af55c3ad7df66e3a1c10d15b02aac
Tom Walsh <hello@tmwlsh.co.uk>
2020-10-01 09:36:17 +0100

    adds german url

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
       </ul>
     </div>
     <div class="lang-select-block" :class="languageBlockOpen ? 'open' : 'closed'">
-      <a v-on:click={toggleLangSelector} href="../de"><img src="~assets/flags/german.png" alt="German Flag" /></a>
+      <a v-on:click={toggleLangSelector} href="../"><img src="~assets/flags/german.png" alt="German Flag" /></a>
       <a v-on:click={toggleLangSelector} href="../fr"><img src="~assets/flags/french.png" alt="French Flag" /></a>
       <a v-on:click={toggleLangSelector} href="../es"><img src="~assets/flags/spanish.png" alt="Spanish Flag" /></a>
     </div>
